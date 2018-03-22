38299e54fc11134b9974736fefa40d53745f6897
Hammie217 <hcsams1@sheffield.ac.uk>
2018-03-22 15:45:02 +0000

    add smoothing and green zone

--- SELABOX.ino
+++ SELABOX.ino
@@ -14,6 +14,9 @@
 #define LED_TYPE    WS2811
 #define COLOR_ORDER GRB
 #define UPDATES_PER_SECOND 100
+#define averagingConstant 20
+#define greenTolerance 2
+#define orangeTolerance 15
 CRGB leds[NUM_LEDS];
 //Global variable init
 double potentiometerVoltage=0;
@@ -38,27 +41,38 @@ void loop() {
 }
 
 void LEDStrip(){
-    leds[(int)potentiometerVoltage] = CRGB::Black;
-  potentiometerVoltage = analogRead(1);
-  delay(1);
-  potentiometerVoltage += analogRead(1);
-  delay(1);
-  potentiometerVoltage += analogRead(1);
-  delay(1);
-  potentiometerVoltage += analogRead(1);
-  delay(1);
-  potentiometerVoltage /=4;
+    leds[(int)potentiometerVoltage] = CRGB::Black; //turn old led off
+    // get average value to remove jumping
+    potentiometerVoltage =0;
+    for(int i =0;i<averagingConstant;i++)
+    {
+        potentiometerVoltage += analogRead(1);
+        delay(1);
+    }
+  //number fiddling to change input into led numbers
+  potentiometerVoltage /=averagingConstant;
   potentiometerVoltage -=320;
   potentiometerVoltage *= NUM_LEDS;
   potentiometerVoltage = round(potentiometerVoltage/350);
+  //if negative make it led 0
   if(potentiometerVoltage<0){
     potentiometerVoltage =0;
   }
-  else if(potentiometerVoltage >143){
-    potentiometerVoltage =143;
+  else if(potentiometerVoltage >NUM_LEDS-1){ //if above end of strip make it the end of the strip
+    potentiometerVoltage =NUM_LEDS-1;
   }
-  Serial.println(potentiometerVoltage);
-  leds[(int)potentiometerVoltage] = CRGB::White;
+  //Serial.println(potentiometerVoltage); //debug line
+  if((potentiometerVoltage>(NUM_LEDS/2)-greenTolerance)&&(potentiometerVoltage<(NUM_LEDS/2)+greenTolerance)){
+
+      leds[(int)potentiometerVoltage] = CRGB::Green;
+  }
+  else if((potentiometerVoltage>(NUM_LEDS/2)-orangeTolerance)&&(potentiometerVoltage<(NUM_LEDS/2)+orangeTolerance)){
+      leds[(int)potentiometerVoltage] = CRGB::Orange;
+  }
+  else{
+    leds[(int)potentiometerVoltage] = CRGB::Red;
+  }
+
   
   
 }
